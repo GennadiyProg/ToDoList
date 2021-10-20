053d89ef59c74d2d720e1d51ded1ea5a1ffb4439
GennadiyProg <grumancev9@gmail.com>
2021-10-20 18:45:06 +0300

    Corrected task-1

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -7,10 +7,11 @@ public class ToDoList {
     public static final String PRINT = "print";
     public static final String TOGGLE = "toggle";
     public static final String QUIT = "quit";
+    static String task = "";
+    static boolean completed = false;
+    static int id = 1;
 
     public static void main(String[] args) throws IOException {
-        String task = "";
-        boolean completed = false;
         while (true){
             System.out.print("Ввод команды: ");
             BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
@@ -21,48 +22,13 @@ public class ToDoList {
             }
             switch (command[0]){
                 case ADD:
-                    StringBuilder sbTask = new StringBuilder();
-                    completed = false;
-                    for (int i = 1; i < command.length; i++){
-                        sbTask.append(command[i]).append(" ");
-                    }
-                    task = sbTask.toString().trim();
+                    add(command);
                     break;
                 case PRINT:
-                    if (command.length == 2 && command[1].equals("all")){
-                        if (task.equals("")) {
-                            System.out.println("Задачи отсутствуют");
-                            break;
-                        }
-                        if(completed){
-                            System.out.println("1. [X] " + task);
-                        } else {
-                            System.out.println("1. [] " + task);
-                        }
-                    } else if (command.length == 1){
-                        if (task.equals("") || completed) {
-                            System.out.println("Задачи отсутствуют");
-                            break;
-                        } else {
-                            System.out.println("Невыполненные задачи:");
-                            System.out.println("1. [] " + task);
-                        }
-                    } else {
-                        System.out.println("Неверные агрументы команды 'print'");
-                    }
+                    print(command);
                     break;
                 case TOGGLE:
-                    if (command.length != 2){
-                        System.out.println("Неверные агрументы команды 'toggle'");
-                    }
-                    int id;
-                    try {
-                        id = Integer.parseInt(command[1]);
-                        break;
-                    } catch (NumberFormatException e) {
-                        System.out.println("Введенное id не является числом");
-                    }
-                    completed = !completed;
+                    toggle(command);
                     break;
                 case QUIT:
                     return;
@@ -70,6 +36,56 @@ public class ToDoList {
                     System.out.println("Введена некорректная команда");
                     break;
             }
+        }
+    }
+
+    private static void add(String[] command){
+        if (command.length == 1){
+            System.out.println("Не было ввода задачи");
+            return;
+        }
+        StringBuilder sbTask = new StringBuilder();
+        completed = false;
+        for (int i = 1; i < command.length; i++){
+            sbTask.append(command[i]).append(" ");
+        }
+        task = sbTask.toString().trim();
+    }
+
+    private static void print(String[] command){
+        if (command.length == 2 && command[1].equals("all")){
+            if (task.equals("")) {
+                System.out.println("Задачи отсутствуют");
+                return;
+            }
+            System.out.printf("%d. [%s] %s%n",id, completed ? "X" : " ", task);
+        } else if (command.length == 1){
+            if (task.equals("") || completed) {
+                System.out.println("Задачи отсутствуют");
+                return;
+            }
+            System.out.println("Невыполненные задачи:");
+            System.out.printf("%d. [ ] %s%n", id, task);
+        } else {
+            System.out.println("Неверные агрументы команды 'print'");
+        }
+    }
+
+    private static void toggle(String[] command){
+        if (command.length != 2){
+            System.out.println("Неверные агрументы команды 'toggle'");
+            return;
+        }
+        int toggleId;
+        try {
+            toggleId = Integer.parseInt(command[1]);
+            if (toggleId == id){
+                completed = !completed;
+            } else {
+                System.out.println("Задачи с таким id не существует");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Введенное id не является числом");
         }
     }
 }
