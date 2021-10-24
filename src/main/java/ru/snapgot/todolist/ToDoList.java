cf8c7afef79fa53c6f7a07ef441d0e87cef24b03
GennadiyProg <grumancev9@gmail.com>
2021-10-24 08:56:02 +0300

    Task-2

--- src/main/java/ru/snapgot/todolist/ToDoList.java
+++ src/main/java/ru/snapgot/todolist/ToDoList.java
@@ -3,15 +3,20 @@ package ru.snapgot.todolist;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ToDoList {
     public static final String ADD = "add";
     public static final String PRINT = "print";
     public static final String TOGGLE = "toggle";
     public static final String QUIT = "quit";
-    static String task = "";
-    static boolean completed = false;
-    static int id = 1;
+    public static final String DELETE = "delete";
+    public static final String EDIT = "edit";
+    public static final String SEARCH = "search";
+    public static HashMap<Integer, String> tasks = new HashMap<>();
+    public static HashMap<Integer, Boolean> status = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         while (true){
@@ -29,8 +34,17 @@ public class ToDoList {
                 case PRINT:
                     print(command);
                     break;
+                case SEARCH:
+                    search(command);
+                    break;
                 case TOGGLE:
                     toggle(command);
+                    break;
+                case DELETE:
+                    delete(command);
+                    break;
+                case EDIT:
+                    edit(command);
                     break;
                 case QUIT:
                     return;
@@ -47,30 +61,54 @@ public class ToDoList {
             return;
         }
         StringBuilder sbTask = new StringBuilder();
-        completed = false;
         for (int i = 1; i < command.length; i++){
             sbTask.append(command[i]).append(" ");
         }
-        task = sbTask.toString().trim();
+        int id = tasks.size() == 0 ? 1 : Collections.max(tasks.keySet()) + 1;
+        tasks.put(id, sbTask.toString().trim());
+        status.put(id, false);
     }
 
     private static void print(String[] command){
         if (command.length == 2 && command[1].equals("all")){
-            if (task.equals("")) {
+            if (tasks.isEmpty()) {
                 System.out.println("Задачи отсутствуют");
                 return;
             }
-            System.out.printf("%d. [%s] %s%n",id, completed ? "X" : " ", task);
+            tasks.forEach((id, task) -> System.out.printf("%d. [%s] %s%n", id, status.get(id) ?  "X" : " ", task));
         } else if (command.length == 1){
-            if (task.equals("") || completed) {
+            if (status.entrySet().stream().allMatch(Map.Entry::getValue)){
                 System.out.println("Задачи отсутствуют");
                 return;
             }
-            System.out.println("Невыполненные задачи:");
-            System.out.printf("%d. [ ] %s%n", id, task);
+            tasks.keySet()
+                    .stream()
+                    .filter(id -> !status.get(id))
+                    .forEach(id -> System.out.printf("%d. [ ] %s%n", id, tasks.get(id)));
         } else {
             System.out.println("Неверные агрументы команды 'print'");
         }
+    }
+
+    private static void search(String[] command){
+        if (command.length == 1){
+            System.out.println("Не было ввода подстроки");
+            return;
+        }
+        StringBuilder sbTask = new StringBuilder();
+        for (int i = 1; i < command.length; i++){
+            sbTask.append(command[i]).append(" ");
+        }
+        if (tasks.entrySet()
+                .stream()
+                .noneMatch(task -> task.getValue().contains(sbTask.toString().trim()))){
+            System.out.println("Задач с такое подстрокой нет");
+            return;
+        }
+        tasks.keySet()
+                .stream()
+                .filter(task -> tasks.get(task).contains(sbTask.toString().trim()))
+                .forEach(id -> System.out.printf("%d. [%s] %s%n", id, status.get(id) ?  "X" : " ", tasks.get(id)));
     }
 
     private static void toggle(String[] command){
@@ -81,11 +119,52 @@ public class ToDoList {
         int toggleId;
         try {
             toggleId = Integer.parseInt(command[1]);
-            if (toggleId == id){
-                completed = !completed;
+            if (tasks.keySet().stream().anyMatch(id -> id == toggleId)){
+                status.replace(toggleId, !status.get(toggleId));
             } else {
                 System.out.println("Задачи с таким id не существует");
             }
+        } catch (NumberFormatException e) {
+            System.out.println("Введенное id не является числом");
+        }
+    }
+
+    private static void delete(String[] command){
+        if (command.length != 2){
+            System.out.println("Неверные агрументы команды 'delete'");
+            return;
+        }
+        int deletedId;
+        try {
+            deletedId = Integer.parseInt(command[1]);
+            if (tasks.keySet().stream().anyMatch(id -> id == deletedId)){
+                tasks.remove(deletedId);
+                status.remove(deletedId);
+            } else {
+                System.out.println("Задачи с таким id не существует");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Введенное id не является числом");
+        }
+    }
+
+    private static void edit(String[] command){
+        if (command.length < 3){
+            System.out.println("Неверные агрументы команды 'edit'");
+            return;
+        }
+        int editId;
+        try {
+            editId = Integer.parseInt(command[1]);
+            if (tasks.keySet().stream().noneMatch(id -> id == editId)){
+                System.out.println("Задачи с таким id не существует");
+                return;
+            }
+            StringBuilder sbTask = new StringBuilder();
+            for (int i = 2; i < command.length; i++){
+                sbTask.append(command[i]).append(" ");
+            }
+            tasks.put(editId, sbTask.toString().trim());
         } catch (NumberFormatException e) {
             System.out.println("Введенное id не является числом");
         }
